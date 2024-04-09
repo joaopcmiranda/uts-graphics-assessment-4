f1b016d365d1cac321f762aad329330a09c6ef7f
Theo <theoipicard@gmail.com>
2024-04-09 19:55:22 +1000

    basic coaster tracks

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,35 @@
 import { App } from "./core/App.ts";
 import { startUI } from "./ui/UI.tsx";
 
+import * as THREE from "three";
+import {createCoasterMesh} from "./coaster-mesh.js";
+
 const app = App();
 
 const parameters = startUI();
 
-app.setup(() => {
+app.setup(({scene}) => {
+
+
+  // testing nonsense
+  var path = new THREE.CurvePath();
+  path.add(new THREE.LineCurve3(new THREE.Vector3(-5, 0, 0), new THREE.Vector3(5, 0, 0)));
+  path.add(new THREE.QuadraticBezierCurve3(
+    new THREE.Vector3(5, 0, 0),
+    new THREE.Vector3(10, 0, 0),
+    new THREE.Vector3(10, 5, 10)
+  ));
+
+  var testTubeGeometry = new THREE.TubeGeometry(path, 64, 0.1);
+  var testTubeMat = new THREE.MeshBasicMaterial();
+  testTubeMat.wireframe = true;
+
+  scene.add(new THREE.Mesh(testTubeGeometry, testTubeMat));
+
+  var model = createCoasterMesh(path);
+  scene.add(model);
+
+
 
 
 });
