354ed5786b4ec173f8e40937aa7cca3e3d8ae4fe
Joao Miranda <50178167+joaopcmiranda@users.noreply.github.com>
2024-05-29 18:28:09 +1000

    wip

--- src/terrain/Terrain.ts
+++ src/terrain/Terrain.ts
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { ShaderMaterial } from "three";
 import terrainVertexShader from "./terrain.vert?raw";
 import terrainFragmentShader from "./terrain.frag?raw";
 import functionsShader from "./functions.glsl?raw";
@@ -23,11 +24,10 @@ import stoneNormalTexture from "./texture/stone/Stone_001_NRM.jpg";
 import stoneRoughnessTexture from "./texture/stone/Stone_001_ROUGH.jpg";
 import { Parameters } from "../ui/parameters.ts";
 
-const { PlaneGeometry, TextureLoader, Mesh, MeshPhongMaterial } = THREE;
+const { PlaneGeometry, TextureLoader, Mesh } = THREE;
 type PlaneGeometry = THREE.PlaneGeometry;
-type MeshPhongMaterial = THREE.MeshPhongMaterial;
 
-export class Terrain extends Mesh<PlaneGeometry, MeshPhongMaterial> {
+export class Terrain extends Mesh<PlaneGeometry, ShaderMaterial> {
 
   private p_normalMap: boolean;
   private p_xLength: number;
@@ -51,58 +51,58 @@ export class Terrain extends Mesh<PlaneGeometry, MeshPhongMaterial> {
     const geometry = new PlaneGeometry(parameters.xLength, parameters.zLength, parameters.xLength * parameters.polyCount, parameters.zLength * parameters.polyCount);
 
     console.log('Terrain material');
-    const material = new MeshPhongMaterial({
-      color: 0x00ff00,
-      side: THREE.FrontSide
-    });
-    material.onBeforeCompile = (shader) => {
+    const material = new ShaderMaterial({
+      uniforms: THREE.UniformsUtils.merge([
+        THREE.UniformsLib.common,
+        THREE.UniformsLib.specularmap,
+        THREE.UniformsLib.envmap,
+        THREE.UniformsLib.aomap,
+        THREE.UniformsLib.lightmap,
+        THREE.UniformsLib.fog,
+        THREE.UniformsLib.lights,
+        {
+          uGenerations: { value: parameters.generations },
+          uDecayRate: { value: parameters.amplitudeDecayRate },
+          uRoughness: { value: parameters.roughness },
+          uInitialAmplitude: { value: parameters.scale },
+          uInitialFrequency: { value: parameters.hillDensity },
+          uOffset: { value: parameters.offset },
+          uEdgeOffset: { value: parameters.edgeOffset },
+          uEdgeOffsetThreshold: { value: parameters.edgeOffsetThreshold },
+          uTextureDensity: { value: parameters.uTextureDensity },
+          uGrassSeed: { value: parameters.grassSeed },
+          uStoneThreshold: { value: parameters.stoneThreshold },
+          uFogDistance: { value: parameters.fogDistance },
 
-      shader.uniforms = {
-        ...shader.uniforms,
-        uGenerations: { value: parameters.generations },
-        uDecayRate: { value: parameters.amplitudeDecayRate },
-        uRoughness: { value: parameters.roughness },
-        uInitialAmplitude: { value: parameters.scale },
-        uInitialFrequency: { value: parameters.hillDensity },
-        uOffset: { value: parameters.offset },
-        uEdgeOffset: { value: parameters.edgeOffset },
-        uEdgeOffsetThreshold: { value: parameters.edgeOffsetThreshold },
-        uTextureDensity: { value: parameters.uTextureDensity },
-        uGrassSeed: { value: parameters.grassSeed },
-        uStoneThreshold: { value: parameters.stoneThreshold },
-        uFogDistance: { value: parameters.fogDistance },
-
-        // textures
-        uGrass1Texture: { value: new TextureLoader().load(grass1ColorTexture) },
-        uGrass1TextureNormal: { value: new TextureLoader().load(grass1NormalTexture) },
-        uGrass1TextureRoughness: { value: new TextureLoader().load(grass1RoughnessTexture) },
-        uGrass3Texture: { value: new TextureLoader().load(grass3ColorTexture) },
-        uGrass3TextureNormal: { value: new TextureLoader().load(grass3NormalTexture) },
-        uGrass3TextureRoughness: { value: new TextureLoader().load(grass3RoughnessTexture) },
-        uGrass4Texture: { value: new TextureLoader().load(grass4ColorTexture) },
-        uGrass4TextureNormal: { value: new TextureLoader().load(grass4NormalTexture) },
-        uGrass4TextureRoughness: { value: new TextureLoader().load(grass4RoughnessTexture) },
-        uGrass5Texture: { value: new TextureLoader().load(grass5ColorTexture) },
-        uGrass5TextureNormal: { value: new TextureLoader().load(grass5NormalTexture) },
-        uGrass5TextureRoughness: { value: new TextureLoader().load(grass5RoughnessTexture) },
-        uStoneTexture: { value: new TextureLoader().load(stoneColorTexture) },
-        uStoneTextureNormal: { value: new TextureLoader().load(stoneNormalTexture) },
-        uStoneTextureRoughness: { value: new TextureLoader().load(stoneRoughnessTexture) }
-      }
-
-      shader.defines = {
-        ...shader.defines,
+          // textures
+          uGrass1Texture: { value: new TextureLoader().load(grass1ColorTexture) },
+          uGrass1TextureNormal: { value: new TextureLoader().load(grass1NormalTexture) },
+          uGrass1TextureRoughness: { value: new TextureLoader().load(grass1RoughnessTexture) },
+          uGrass3Texture: { value: new TextureLoader().load(grass3ColorTexture) },
+          uGrass3TextureNormal: { value: new TextureLoader().load(grass3NormalTexture) },
+          uGrass3TextureRoughness: { value: new TextureLoader().load(grass3RoughnessTexture) },
+          uGrass4Texture: { value: new TextureLoader().load(grass4ColorTexture) },
+          uGrass4TextureNormal: { value: new TextureLoader().load(grass4NormalTexture) },
+          uGrass4TextureRoughness: { value: new TextureLoader().load(grass4RoughnessTexture) },
+          uGrass5Texture: { value: new TextureLoader().load(grass5ColorTexture) },
+          uGrass5TextureNormal: { value: new TextureLoader().load(grass5NormalTexture) },
+          uGrass5TextureRoughness: { value: new TextureLoader().load(grass5RoughnessTexture) },
+          uStoneTexture: { value: new TextureLoader().load(stoneColorTexture) },
+          uStoneTextureNormal: { value: new TextureLoader().load(stoneNormalTexture) },
+          uStoneTextureRoughness: { value: new TextureLoader().load(stoneRoughnessTexture) }
+        }
+      ]),
+      defines: {
         WIDTH: (parameters.xLength * parameters.polyCount).toFixed(1),
         HEIGHT: (parameters.zLength * parameters.polyCount).toFixed(1),
         BOUNDSX: parameters.xLength.toFixed(1),
         BOUNDSY: parameters.zLength.toFixed(1),
         DEFAULT_SEED: parameters.terrainSeed.toFixed(6),
         SHOW_NORMAL_MAP: parameters.normalMap
-      }
-
-      shader.fragmentShader = functionsShader + terrainFragmentShader;
-      shader.vertexShader = functionsShader + terrainVertexShader;
-    };
+      },
+      fragmentShader: functionsShader + terrainFragmentShader,
+      vertexShader: functionsShader + terrainVertexShader
+    });
 
     console.log('Terrain super');
 
@@ -141,60 +141,60 @@ export class Terrain extends Mesh<PlaneGeometry, MeshPhongMaterial> {
     }
 
     if (this.p_terrainSeed !== newParameters.terrainSeed) {
-      this.material.userData.shader.defines.DEFAULT_SEED = newParameters.terrainSeed.toFixed(6);
+      this.material.defines.DEFAULT_SEED = newParameters.terrainSeed.toFixed(6);
       this.material.needsUpdate = true;
       this.p_terrainSeed = newParameters.terrainSeed;
     }
     if (this.p_generations !== newParameters.generations) {
-      this.material.userData.shader.uniforms.uGenerations.value = newParameters.generations;
+      this.material.uniforms.uGenerations.value = newParameters.generations;
       this.p_generations = newParameters.generations;
     }
     if (this.p_amplitudeDecayRate !== newParameters.amplitudeDecayRate) {
-      this.material.userData.shader.uniforms.uDecayRate.value = newParameters.amplitudeDecayRate;
+      this.material.uniforms.uDecayRate.value = newParameters.amplitudeDecayRate;
       this.p_amplitudeDecayRate = newParameters.amplitudeDecayRate;
     }
     if (this.p_roughness !== newParameters.roughness) {
-      this.material.userData.shader.uniforms.uRoughness.value = newParameters.roughness;
+      this.material.uniforms.uRoughness.value = newParameters.roughness;
       this.p_roughness = newParameters.roughness;
     }
     if (this.p_scale !== newParameters.scale) {
-      this.material.userData.shader.uniforms.uInitialAmplitude.value = newParameters.scale;
+      this.material.uniforms.uInitialAmplitude.value = newParameters.scale;
       this.p_scale = newParameters.scale;
     }
     if (this.p_hillDensity !== newParameters.hillDensity) {
-      this.material.userData.shader.uniforms.uInitialFrequency.value = newParameters.hillDensity;
+      this.material.uniforms.uInitialFrequency.value = newParameters.hillDensity;
       this.p_hillDensity = newParameters.hillDensity;
     }
     if (this.p_offset !== newParameters.offset) {
-      this.material.userData.shader.uniforms.uOffset.value = newParameters.offset;
+      this.material.uniforms.uOffset.value = newParameters.offset;
       this.p_offset = newParameters.offset;
     }
     if (this.p_edgeOffset !== newParameters.edgeOffset) {
-      this.material.userData.shader.uniforms.uEdgeOffset.value = newParameters.edgeOffset;
+      this.material.uniforms.uEdgeOffset.value = newParameters.edgeOffset;
       this.p_edgeOffset = newParameters.edgeOffset;
     }
     if (this.p_edgeOffsetThreshold !== newParameters.edgeOffsetThreshold) {
-      this.material.userData.shader.uniforms.uEdgeOffsetThreshold.value = newParameters.edgeOffsetThreshold;
+      this.material.uniforms.uEdgeOffsetThreshold.value = newParameters.edgeOffsetThreshold;
       this.p_edgeOffsetThreshold = newParameters.edgeOffsetThreshold;
     }
     if (this.p_uTextureDensity !== newParameters.uTextureDensity) {
-      this.material.userData.shader.uniforms.uTextureDensity.value = newParameters.uTextureDensity;
+      this.material.uniforms.uTextureDensity.value = newParameters.uTextureDensity;
       this.p_uTextureDensity = newParameters.uTextureDensity;
     }
     if (this.p_grassSeed !== newParameters.grassSeed) {
-      this.material.userData.shader.uniforms.uGrassSeed.value = newParameters.grassSeed;
+      this.material.uniforms.uGrassSeed.value = newParameters.grassSeed;
       this.p_grassSeed = newParameters.grassSeed;
     }
     if (this.p_stoneThreshold !== newParameters.stoneThreshold) {
-      this.material.userData.shader.uniforms.uStoneThreshold.value = newParameters.stoneThreshold;
+      this.material.uniforms.uStoneThreshold.value = newParameters.stoneThreshold;
       this.p_stoneThreshold = newParameters.stoneThreshold;
     }
     if (this.p_fogDistance !== newParameters.fogDistance) {
-      this.material.userData.shader.uniforms.uFogDistance.value = newParameters.fogDistance;
+      this.material.uniforms.uFogDistance.value = newParameters.fogDistance;
       this.p_fogDistance = newParameters.fogDistance;
     }
     if (this.p_normalMap !== newParameters.normalMap) {
-      this.material.userData.shader.defines.SHOW_NORMAL_MAP = newParameters.normalMap;
+      this.material.defines.SHOW_NORMAL_MAP = newParameters.normalMap;
       this.material.needsUpdate = true;
       this.p_normalMap = newParameters.normalMap;
     }
